401552f22ad0b512fdefcca0ee440683a4644da4
Aaron1119 <crisostomoaaronluis_bsit@plmun.edu.ph>
2025-02-27 22:30:12 +0800

    Submitting Python Session 03 - Exercise 01

--- problem_01.py
+++ problem_01.py
@@ -4,25 +4,13 @@
 # Simple Calculator Program
 
 def main():
-<<<<<<< HEAD
     print("Simple Calculator Program")
-    
     num1 = int(input("Enter 1st number: "))
     num2 = int(input("Enter 2nd number: "))
-
     print(f"The sum is {num1 + num2}")
     print(f"The difference is {num1 - num2}")
     print(f"The product is {num1 * num2}")
     print(f"The quotient is {num1 / num2:.2f}")
-=======
-     	print("Simple Calculator Program")
-	num1 = int(input("Enter 1st number: "))
-	num2 = int(input("Enter 2nd number: "))
-	print(f"The sum is {num1 + num2}")
-	print(f"The difference is {num1 - num2}")
-	print(f"The product is {num1 * num2}")
-	print(f"The quotient is {(num1 / num2):.2f}")
->>>>>>> 613cabcc225e9981643c1286b696db49e70d5cba
 
 if __name__ == "__main__":
     main()
